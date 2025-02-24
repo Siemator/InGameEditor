26beb19ae52dfe53281e51fc59e567ca92674e65
Siemator <siematorpl@gmail.com>
2025-02-24 19:42:39 +0100

    Try to add grid system

--- Assets/Script/EditorManager/EditorManager.cs
+++ Assets/Script/EditorManager/EditorManager.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class EditorManager : MonoBehaviour
 {
     public GameObject statsGraph;
+    public Transform grindParent;  // This will be the parent for grid points and lines
+    public bool drawGrid = false;
 
     private bool grapStatsu = false;
 
     void Start()
     {
-
+        // Create a grid of 2 x 2 cells (adjust as needed)
+        Grid grid = new Grid(12, 12, grindParent);
     }
 
     void Update()
     {
-
+        // Your other update code…
     }
 
     public void statsSwitch()
